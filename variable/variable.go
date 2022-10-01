54c2d1e36f432a4d8678eaaa221ad24ef43d33e9
FarBb <loveinsemarang@gmail.com>
2022-10-01 16:11:44 +0700

    Variable

---
+++ variable/variable.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+	name = "Faris"
+
+	fmt.Println(name)
+
+	// tipe data integer
+	var age = 20
+	fmt.Println(age)
+
+	// tipe data string
+	var namaku = "Mochammad Faris"
+	fmt.Println(namaku)
+	
+	dataku := "Faris"
+	fmt.Println(dataku)
+
+	// deklarasi multiple
+	var (
+		firstname = "Mochammad"
+		lastname = "Faris"
+	)
+
+	fmt.Println(firstname)
+	fmt.Println(lastname)
+}
